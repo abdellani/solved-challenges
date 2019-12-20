c2d15197d78c10ac1e5cc8854b3ca6c6fd0c9053
Mohamed ABDELLANI <abdellani@gmail.com>
2019-12-20 18:49:04 +0100

    Create Weather Observation Station 12.sql

---
+++ hackerrank/sql/Weather Observation Station 12.sql
@@ -0,0 +1,7 @@
+/*
+source: https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+*/
+
+select distinct(city) from station where 
+lower(substr(city,1,1)) not in ('a','e','u','i','o')
+and lower(substr(city,length(city),1)) not in ('a','e','u','i','o')
