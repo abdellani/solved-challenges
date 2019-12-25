dcdea3f395ac924076cf7df62e5c5f3594374d3b
Mohamed ABDELLANI <abdellani@gmail.com>
2019-12-26 00:26:01 +0100

    Update Max Array Sum.rb

--- hackerrank/dp/Max Array Sum.rb
+++ hackerrank/dp/Max Array Sum.rb
@@ -4,15 +4,11 @@ require 'json'
 require 'stringio'
 
 def maxSubsetSum(arr)
-    dp=[]
-    dp<< arr.first
-    dp<< arr[0..1].max
-    ans= dp.max
+    dp=[arr.first,arr[0..1].max]
     arr[2..arr.length].each do |el|
-        dp<< [[dp[-2]+el,el].max,ans].max
-        ans= [ans,dp.last].max
+        dp<< [[dp[-2]+el,el].max,dp[-1]].max
     end
-    ans
+    dp.last
 end
 
 fptr = File.open(ENV['OUTPUT_PATH'], 'w')
