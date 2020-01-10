07d1e00299279c054215131ae028112f43f96dfa
Mohamed ABDELLANI <abdellani@gmail.com>
2020-01-10 15:50:27 +0100

    Update 230. Kth Smallest Element in a BST.rb

--- leetcode/230. Kth Smallest Element in a BST.rb
+++ leetcode/230. Kth Smallest Element in a BST.rb
@@ -12,20 +12,24 @@
 # @param {Integer} k
 # @return {Integer}
 
-def visit_node(node,hash)
-    visit_node(node.left, hash) if node.left
-    hash[:counter]+=1    
-    hash[:found] = true and hash[:kth_element]= node.val if hash[:counter] == hash[:k]
-    visit_node(node.right,hash) if !hash[:found] and  node.right
-end
+def inorder( node, hash)
+    # visit left child, visit right child
+    inorder(node.left, hash) if node.left
+    hash[:counter]+=1
+    if hash[:counter]== hash[:k]
+        hash[:kthelement]=node.val
+        hash[:found]=true
+    end
+    inorder(node.right, hash) if hash[:found] == false and node.right
 
+end
 def kth_smallest(root, k)
-  hash={
-      k: k,
-      counter: 0,
-      kth_element: nil,
-      found: false
-  }
-  visit_node(root,hash)
-  return hash[:kth_element]  
+    hash={
+        k:k,
+        counter:0,
+        found:false,
+        kthelement:nil   
+    }
+    inorder(root, hash)
+    hash[:kthelement]
 end
